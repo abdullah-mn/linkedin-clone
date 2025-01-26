85dc4dbbf4d03de12cbf44df18f1370bc865d59b
abda <abdullahabda222@gmail.com>
2025-01-26 13:24:09 +0530

    Add MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
@@ -11,4 +13,5 @@ app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
