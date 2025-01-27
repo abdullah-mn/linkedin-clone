bf0c6fa73ee1ae1c41a6c625d91fdb507c76aed9
abda <abdullahabda222@gmail.com>
2025-01-27 12:21:54 +0530

    feat: Implement login logic with validation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -56,6 +56,31 @@ export const signup = async (req, res) => {
   }
 };
 
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: "Please fill all fields" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.status(200).json({message: "Logged in successfully"})
+    
+  } catch (error) {
+    console.log("Error in login ", error.message);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
 export const logout = async (req, res) => {
   res.clearCookie("jwt-linkedin");
   res.json({ message: "Logged out successfully" });
