5b744f65ad5ca2f3b52b443252cf7fa210238044
abda <abdullahabda222@gmail.com>
2025-01-27 23:58:08 +0530

    feat: Add '/current-user' route to fetch authenticated user

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { signup, logout, login } from "../controllers/auth.controller.js";
+import { getCurrentUser } from "../controllers/auth.controller.js";
+import protectRoute from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.get("/current-user", protectRoute, getCurrentUser);
 
 export default router;
