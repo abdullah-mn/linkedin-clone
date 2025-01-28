e66ee2d9b671428ab796f387085d01d6cbc7e3b9
abda <abdullahabda222@gmail.com>
2025-01-29 00:26:55 +0530

    fix: Import missing getFeedPosts from post controller

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { getFeedPosts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
