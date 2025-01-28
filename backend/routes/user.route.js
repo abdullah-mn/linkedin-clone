843d14c197da7cdcf90de3ed57c6bce81dfad336
abda <abdullahabda222@gmail.com>
2025-01-28 14:48:07 +0530

    feat: Add '/:username' route to fetch public profile of user

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getSuggestedConnections } from "../controllers/user.controller.js";
+import {
+  getSuggestedConnections,
+  getPublicProfile,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/suggested-connections", protectRoute, getSuggestedConnections);
+router.get("/:username", protectRoute, getPublicProfile);
 
 export default router;
