1f9641cf320f0eb38b275790a8855ab1edf2b113
abda <abdullahabda222@gmail.com>
2025-01-28 21:30:26 +0530

    feat: Add '/profile' route to update user profile

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,11 +3,14 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getSuggestedConnections,
   getPublicProfile,
+  updateProfile,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.get("/suggested-connections", protectRoute, getSuggestedConnections);
 router.get("/:username", protectRoute, getPublicProfile);
+
+router.put("/profile", protectRoute, updateProfile);
 
 export default router;
