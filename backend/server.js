c59397b50b1c70c7fba20c03965de3fbc44bbeb6
abda <abdullahabda222@gmail.com>
2025-01-28 23:45:19 +0530

    feat: Add postRoutes in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -16,6 +17,7 @@ app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/post", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
