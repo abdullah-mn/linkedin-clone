4f1f236ecaf4543890100807329a3ea245a0cda5
abda <abdullahabda222@gmail.com>
2025-01-28 23:50:23 +0530

    feat: Implement getFeedPosts logic in post controller

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,15 @@
+import Post from "../models/post.model.js";
+
+export const getFeedPosts = async (req, res) => {
+  try {
+    const posts = await Post.find({ author: { $in: req.user.connections } })
+      .populate("author", "name username profilePicture headline")
+      .populate("comments.user", "name profilePicture")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json(posts);
+  } catch (error) {
+    console.log("Error in getFeedPosts controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
