d6a609e2694bee1026a526d45076e0912ece817a
abda <abdullahabda222@gmail.com>
2025-01-29 00:32:22 +0530

    feat: Implement createPost logic and upload post image to cloudinary

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Post from "../models/post.model.js";
 
 export const getFeedPosts = async (req, res) => {
@@ -10,6 +11,33 @@ export const getFeedPosts = async (req, res) => {
     res.status(200).json(posts);
   } catch (error) {
     console.log("Error in getFeedPosts controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const { content, image } = req.body;
+
+    let newPost;
+    if (image) {
+      const imgResult = await cloudinary.uploader.upload(image);
+      newPost = new Post({
+        author: req.user._id,
+        image: imgResult.secure_url,
+        content,
+      });
+    } else {
+      newPost = new Post({
+        author: req.user._id,
+        content,
+      });
+    }
+
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.log("Error in createPost controller: ", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
