619aea68ccdc4b46d65104a59fd558c6bbc87c6c
abda <abdullahabda222@gmail.com>
2025-01-30 19:44:01 +0530

    feat: Add '/:id/read' route to mark the notifications read by user

--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware";
-import { getNotifications } from "../controllers/notification.controller";
+import {
+  getNotifications,
+  markNotificationAsRead,
+} from "../controllers/notification.controller";
 
 const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
+router.put("/:id/read", protectRoute, markNotificationAsRead);
 
 export default router;
