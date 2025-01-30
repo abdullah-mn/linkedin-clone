9965e9d930a2e8e5402e8aca3d97c8b079c7eaf6
abda <abdullahabda222@gmail.com>
2025-01-30 19:45:22 +0530

    feat: Implement markNotificationAsRead logic in notification controller

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -13,3 +13,19 @@ export const getNotifications = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const markNotificationAsRead = async (req, res) => {
+  try {
+    const notificationId = req.params.id;
+    const notification = await Notification.findByIdAndUpdate(
+      { _id: notificationId, recipient: req.user._id },
+      { read: true },
+      { new: true }
+    );
+
+    res.status(200).json(notification);
+  } catch (error) {
+    console.log("Error in markNotificationAsRead controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
