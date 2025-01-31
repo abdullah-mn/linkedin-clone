7d8644f0e6bb8bbbd7fb20b75adc7d32a17fcfce
abda <abdullahabda222@gmail.com>
2025-01-31 19:29:42 +0530

    feat: Implement acceptConnectionRequest logic in connection controller

--- backend/controllers/connection.controller.js
+++ backend/controllers/connection.controller.js
@@ -1,4 +1,6 @@
 import ConnectionRequest from "../models/connectionRequest.model.js";
+import User from "../models/user.model.js";
+import Notification from "../models/notification.model.js";
 
 export const sendConnectionRequest = async (req, res) => {
   try {
@@ -35,6 +37,52 @@ export const sendConnectionRequest = async (req, res) => {
     res.status(200).json(newRequest);
   } catch (error) {
     console.log("Error in sendConnectionRequest controller: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const acceptConnectionRequest = async (req, res) => {
+  try {
+    const requestId = req.params.id;
+    const userId = req.user._id;
+
+    const request = await ConnectionRequest.findById(requestId);
+    if (!request) {
+      return res.status(404).json({ message: "Connection request not found" });
+    }
+
+    // check if the request is for the current user
+    if (request.recipient._id.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "Not authorized to accept this request" });
+    }
+
+    if (request.status !== "pending") {
+      return res
+        .status(400)
+        .json({ message: "This request has already been accepted" });
+    }
+    request.status = "accepted";
+    await request.save();
+
+    await User.findByIdAndUpdate(request.sender._id, {
+      $addToSet: { connections: userId },
+    });
+    await User.findByIdAndUpdate(userId, {
+      $addToSet: { connections: request.sender._id },
+    });
+
+    const notification = new Notification({
+      recipient: request.user._id,
+      type: "connectionAccepted",
+      relatedUser: userId,
+    });
+    await notification.save();
+
+    res.status(200).json({ message: "Connection accepted successfully" });
+  } catch (error) {
+    console.log("Error in acceptConnectionRequest controller: ", error.message);
     res.status(500).json({ message: "Internal server error" });
   }
 };
