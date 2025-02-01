890d49f3f6e084aa2f815d5a21de27fc61a4ac3e
abda <abdullahabda222@gmail.com>
2025-02-01 10:56:58 +0530

    feat: Add limit to payload size

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(express.json()); // parse JSON request bodies
+// limit the payload (images) size
+app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
