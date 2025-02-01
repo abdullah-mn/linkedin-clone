de4d60f6bffde971b4e227768bf56af96b1fca75
abda <abdullahabda222@gmail.com>
2025-02-01 15:45:11 +0530

    feat: Setup cors config in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -13,6 +15,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http:/localhost:5173/",
+    credentials: true,
+  })
+);
 // limit the payload (images) size
 app.use(express.json({ limit: "5mb" })); // parse JSON request bodies
 app.use(cookieParser());
