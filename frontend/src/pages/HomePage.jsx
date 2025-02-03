12b4d4db9de4483aa940a098dc8e852faaed843e
abda <abdullahabda222@gmail.com>
2025-02-03 13:35:37 +0530

    chore: Remove debugging console.log in HomePage component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -24,8 +24,6 @@ const HomePage = () => {
     enabled: !!authUser,
   });
 
-  console.log(suggestedConnections);
-
   return (
     <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
       <div className="hidden lg:block lg:col-span-1">
