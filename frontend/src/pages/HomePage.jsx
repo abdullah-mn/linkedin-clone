d0f2bd1c235b39c79f9e2090faec4c86b72b197c
abda <abdullahabda222@gmail.com>
2025-02-03 14:09:02 +0530

    feat: Add Post component to HomePage component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "../lib/axios.js";
 import Sidebar from "../components/Sidebar";
 import PostCreation from "../components/PostCreation.jsx";
+import Post from "../components/Post.jsx";
+import { Users } from "lucide-react";
+import SuggestedConnections from "../components/SuggestedConnections.jsx";
 
 const HomePage = () => {
   const { data: authUser } = useQuery({ queryKey: ["authUser"] });
@@ -31,6 +34,24 @@ const HomePage = () => {
       </div>
       <div className="col-span-1 lg:col-span-2 order-first lg:order-none">
         <PostCreation user={authUser} />
+
+        {posts?.map((post) => (
+          <Post key={post.key} post={post} />
+        ))}
+
+        {posts?.length === 0 && (
+          <div className="bg-white rounded-lg shadow p-8 text-center">
+            <div className="mb-6">
+              <Users size={64} className="mx-auto text-blue-500" />
+            </div>
+            <h2 className="text-2xl font-bold mb-4 text-gray-800">
+              No Posts Yet
+            </h2>
+            <p className="text-gray-600 mb-6">
+              Connect with others to start seeing posts in your feed!
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
