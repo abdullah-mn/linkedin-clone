fc32e7e1e3603cedfaa018d32d528f2322a6fd64
abda <abdullahabda222@gmail.com>
2025-02-05 12:09:07 +0530

    feat: Add route to NetworkPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import toast, { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "./lib/axios";
 import NotificationPage from "./pages/NotificationPage";
+import NetworkPage from "./pages/NetworkPage";
 
 function App() {
   const { data: authUser, isLoading } = useQuery({
@@ -46,6 +47,10 @@ function App() {
         <Route
           path="/notification"
           element={authUser ? <NotificationPage /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/network"
+          element={authUser ? <NetworkPage /> : <Navigate to={"/login"} />}
         />
       </Routes>
       <Toaster />
