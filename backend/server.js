90e1df4cc036061909dd8f1e4f9303e292242d51
abda <abdullahabda222@gmail.com>
2025-02-12 19:38:13 +0530

    fix: Correct 'path.join()' to use relative path to ensure compatibility across systems

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ app.use("/api/v1/notification", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
